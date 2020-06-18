035ac904e346e7032cb3f0b2baa54aeb2a7be85b
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 18:13:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -13,3 +13,14 @@ else
   true 
 end 
 end 
+#def my_all?(collection)
+#i = 0 
+#block_return_values=[]
+#while i < collection.length 
+#block_return_values << yield(collection[i])
+#i += 1 
+#end 
+#
+#
+#
+#
